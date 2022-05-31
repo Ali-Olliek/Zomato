8b3b7d72ce54c8d998001bfdd41d46a743bf402b
Ali-olliek <you@example.com>
2022-05-31 23:00:27 +0300

    trying to fix login

--- script/login.js
+++ script/login.js
@@ -1,28 +1,33 @@
 let button = document.getElementById("submit");
 button.addEventListener("click", function(event){
+
     event.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("pwd").value;
+    console.log(password, " ", username)
 
 
     let data = new FormData();
     data.append('username', username);
     data.append('password', password);
 
-    let url = 'http://localhost/Zomato/ZomatoBackEnd/LoginP.php'; //specifiy where to go after submitting data?
-    let id = localStorage.getItem("user_id");
-    url += "?user_id=" + id;
+    let url = 'http://localhost/Group_Project/group-project-backend/loginP.php'; //specifiy where to go after submitting data?
+    let id = localStorage.getItem("idUsers");
+    url += "?idUsers=" + id;
 
     axios({
-        method: 'GET',
-        url: url
+        method: 'POST',
+        url: url,
+        data: data,
     })
     .then(function(response){
-        let result = response.data;
-        let message = result.status;
-        let id = result.user_id;
-        if(message == "Welcome"){
-            document.getElementById("status").style.display = "block";
+        
+        if (response === "Logged in") {
+          console.log("logged in");
+          var get_id = response.data[0]
+          window.location.replace("http:/Group_Project/Zomato/html/restaurants_display.html");
+        } else {
+          console.log("user not found");
         }
     })
 })
